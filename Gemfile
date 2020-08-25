7d38b694fa7c114e4b46d00b3848b9a6d9ae3859
k.wakitani <k.wakitani@gmail.com>
2020-08-25 23:36:03 +0900

    Add initial working code for now

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+ruby "2.7.1"
+
+gem "ikku", "~> 0.1.4"
+gem "rss", "~> 0.2.9"
+gem "twitter", "~> 7.0"
---
+++ app.rb
@@ -0,0 +1,60 @@
+require "ikku"
+require "logger"
+require "open-uri"
+require "rss"
+require "time"
+require "twitter"
+
+reviewer = Ikku::Reviewer.new
+
+urls = %w[
+  https://www.nhk.or.jp/rss/news/cat0.xml
+  https://www.news24.jp/rss/index.rdf
+  https://www.asahi.com/rss/asahi/newsheadlines.rdf
+  http://www3.asahi.com/rss/animal.rdf
+  https://mainichi.jp/rss/etc/mainichi-flash.rss
+  https://assets.wor.jp/rss/rdf/nikkei/news.rdf
+  https://assets.wor.jp/rss/rdf/reuters/top.rdf
+  https://assets.wor.jp/rss/rdf/ynnews/news.rdf
+]
+  #https://assets.wor.jp/rss/rdf/yomiuri/latestnews.rdf
+
+last = Time.now - 10 * 60 # 10 min
+
+logger = Logger.new(STDERR)
+
+articles = urls.flat_map do |url|
+  logger.debug(url)
+  feed = RSS::Parser.parse(URI.open(url)) rescue next
+  feed.items.select { |i| i.date > last }.map { |i| [i.title, i.link] }
+end
+
+# Do some cleansing
+articles.each do |article|
+  article[0].gsub!(/　\d+\/\d+ \d+:\d+更新$|^[^：:]{,10}[：:]|=[^=]{,10}$|^【[^】]+】|^[^）)]{,10}[）)]/, '')
+  article[1].gsub!(/\?.*$/, '')
+end
+logger.info(articles)
+
+detected = articles.select do |article|
+  song = reviewer.find(article[0])
+  article.push(song.phrases.join) if song
+end
+logger.info(detected)
+
+twitter = Twitter::REST::Client.new do |config|
+  config.consumer_key = ENV["TWITTER_CONSUMER_KEY"]
+  config.consumer_secret = ENV["TWITTER_CONSUMER_SECRET"]
+  config.access_token = ENV["TWITTER_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+end
+
+detected.each do |article|
+  begin
+    tweet = twitter.update("#{article[2]} #{article[1]}")
+  rescue => e
+    logger.error(e)
+  else
+    logger.info(tweet.text)
+  end
+end
